fd67548955d3d54e02e1a9238c0e528a45799a81
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-30 13:22:38 -0400

    Initial Commit

---
+++ vehicles/src/main/java/com/pluralsight/vehicles/Vehicles.java
@@ -0,0 +1,41 @@
+package com.pluralsight.vehicles;
+
+public class Vehicles {
+    String color;
+    int numberOfPasengers;
+    int cargoCapacity;
+    int fuelCapacity;
+    
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPasengers() {
+        return numberOfPasengers;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNumberOfPasengers(int numberOfPasengers) {
+        this.numberOfPasengers = numberOfPasengers;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+}
