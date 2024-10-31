35f19927fe7f4f092ae3a427af9196210b3edb5b
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-31 11:41:19 -0400

    Cleaned up the Sales Contract construcotr

--- car-dealership/src/main/java/com/pluralsight/cardealership/SalesContract.java
+++ car-dealership/src/main/java/com/pluralsight/cardealership/SalesContract.java
@@ -11,8 +11,24 @@ public class SalesContract extends Contract{
     public SalesContract(String startDate, String customerName, String customerEmail, int customerId, Vehicle vehicle,
                          double totalPrice, double monthlyPayment, boolean isFinanced, double annualInterestRate) {
         super(startDate, customerName, customerEmail, customerId, vehicle, totalPrice, monthlyPayment);
-        Utilities.initializeSalesContract(this, vehicle, isFinanced);
+        if (vehicle.getPrice() < 10000){
+            this.processingFee = 295.00;
+        }
+        else {
+            this.processingFee = 495.00;
+        }
+        this.isFinanced = isFinanced;
+        if (isFinanced && vehicle.getPrice() >= 10000) {
+            this.annualInterestRate = 0.0425;
+            this.loanTerm = 24;
+        } else if (isFinanced && vehicle.getPrice() < 10000) {
+            this.annualInterestRate = 0.0525;
+            this.loanTerm = 48;
+        } else {
+            this.annualInterestRate = 0.0;
+        }
     }
+
     @Override
     public double getTotalPrice() {
         double totalPrice = getVehicle().getPrice() + (getVehicle().getPrice() * salesTax) + recordingFee + processingFee;
